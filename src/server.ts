583a4cee5337a5e82eb8337f07d58bde91f54de2
Ariel <arielah1997@gmail.com>
2023-12-25 23:28:57 +0200

    refactor: port changed

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 const app = express();
 
-const PORT = process.env.PORT || 6969;
+const PORT = process.env.PORT || 80;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
