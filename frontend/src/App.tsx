9fcac84e2e09f0f37742a8914a97f9381b14cbac
Ariel <arielah1997@gmail.com>
2023-12-27 22:45:18 +0200

    style: marry xmas!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,25 @@
+import Snowfall from "react-snowfall";
 import DisplayLinks from "./components/links/display-links";
 import Footer from "./components/ui/footer";
 import NavigationBar from "./components/ui/nav";
 
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center flex-col w-screen">
-      <NavigationBar />
-      <main className="w-full md:p-0 p-2">
-        <DisplayLinks />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Snowfall
+        snowflakeCount={140}
+        speed={[0.5, 0.5]}
+        wind={[-0.5, -0.5]}
+        radius={[0.5, 1]}
+      />
+      <div className="h-screen flex items-center justify-center flex-col w-screen">
+        <NavigationBar />
+        <main className="w-full md:p-0 p-2">
+          <DisplayLinks />
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 }
 
